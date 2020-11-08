4358bd68a0099f2db5732875423dc2a513435de5
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-11-09 07:48:07 +0800

    Created frontend directory and moved stuff inside

---
+++ frontend/pages/products/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+const Products = () => {
+  return (
+    <React.Fragment>
+      <Layout>
+        <div>Products</div>
+      </Layout>
+    </React.Fragment>
+  );
+};
+
+export default Products;
